bcd06c1cea1c15385499481d9bedff4fad447140
Pat Sabpisal <ecegrid@gmail.com>
2017-08-05 21:02:17 -0400

    total pakc

--- libs/KerryLogistic.js
+++ libs/KerryLogistic.js
@@ -1,14 +1,17 @@
 class KerryLogistic {
 
-    constructor(){
-        this.price_steps = [30, 65, 85, 100, 140, 185, 290, 380];
+    static PRICE_UPCOUNTRY = [45, 80, 100, 115, 155, 205, 330, 420];
+    static PRICE_URBAN = [30, 65, 85, 100, 140, 185, 290, 380];
+
+    constructor(price = KerryLogistic.PRICE_URBAN) {
+        this.price_steps = price;
     }
 
-    _getPerim(width, height, length, q){
+    _getPerim(width, height, length, q) {
         return Number(q) * height + length + width;
     }
 
-    _pricify(perim, w){
+    _pricify(perim, w) {
         let steps = this.price_steps;
         if (perim <= 40 && w <= 1) {
             return steps[0];
@@ -31,51 +34,51 @@ class KerryLogistic {
         }
     }
 
-    
-    getCostFunction(){
+
+    getCostFunction() {
         let _ = this;
         /*
-        *  (w) - total weight
-        *  (cart) - cart list 
-        */
+         *  (w) - total weight
+         *  (cart) - cart list 
+         */
         return function (cart) {
             //w is total weight
-            let w = cart.map(function(c){
-                return Number(c.sku.weight)*Number(c.quantity)
-            }).reduce(function(p, c){
+            let w = cart.map(function (c) {
+                return Number(c.sku.weight) * Number(c.quantity)
+            }).reduce(function (p, c) {
                 return p + c
             }, 0);
             console.log("Weight", w, "kg");
             //f is the factor for how many box to split
             let f = 1;
             let breakdown = {};
-            let total = cart.reduce(function(p, c){
-                    if (!c.sku.weighted) {
-                        return p;
+            let total = cart.reduce(function (p, c) {
+                if (!c.sku.weighted) {
+                    return p;
+                }
+                let perim_cm = _._getPerim(c.sku.kdw_mm, c.sku.kdh_mm, c.sku.kdl_mm, c.quantity) / 10;
+                let pf = _._pricify(perim_cm);
+
+                //breakdown into smaller boxes
+                while (pf === -1) {
+                    //try until package is divisible (condition is price is valid)
+                    perim_cm = _._getPerim(c.sku.kdw_mm, c.sku.kdh_mm, c.sku.kdl_mm, Number(c.quantity) / parseFloat(f)) / 10;
+                    pf = _._pricify(perim_cm, w / parseFloat(f));
+                    if (f > 10000) {
+                        console.log("No Solution", f);
+                        return;
                     }
-                    let perim_cm = _._getPerim(c.sku.kdw_mm, c.sku.kdh_mm, c.sku.kdl_mm, c.quantity) / 10;
-                    let pf = _._pricify(perim_cm);
+                    f++;
+                }
 
-                    //breakdown into smaller boxes
-                    while (pf === -1) {
-                        //try until package is divisible (condition is price is valid)
-                        perim_cm = _._getPerim(c.sku.kdw_mm, c.sku.kdh_mm, c.sku.kdl_mm, Number(c.quantity) / parseFloat(f)) / 10;
-                        pf = _._pricify(perim_cm, w / parseFloat(f));
-                        if (f > 10000) {
-                            console.log("No Solution", f);
-                            return;
-                        }
-                        f++;
-                    }
+                breakdown = {
+                    pricePerPack: pf,
+                    totalPacks: f - 1,
+                    perimeterPerPack: perim_cm.toFixed(2),
+                    WeightPerPack: (w / parseFloat(f)).toFixed(2)
+                };
 
-                    breakdown = {
-                        pricePerPack: pf,
-                        totalPack: f-1,
-                        perimeterPerPack: perim_cm.toFixed(2),
-                        WeightPerPack: (w/parseFloat(f)).toFixed(2)
-                    };
-
-                    return p + pf * (f-1)
+                return p + pf * (f - 1)
             }, 0);
 
             return {
@@ -87,7 +90,7 @@ class KerryLogistic {
 }
 
 /*
-*
-* Steps is the KERRY pricing step
-*/
+ *
+ * Steps is the KERRY pricing step
+ */
 module.exports = KerryLogistic;
